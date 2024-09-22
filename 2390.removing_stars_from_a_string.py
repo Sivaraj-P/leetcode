d0b2bcab2bd516f49ffadf25232346ca5b960166
Sivaraj-P <sivarajpauldurai@gmail.com>
2024-09-22 21:55:14 +0530

    removing_stars_from_a_string

---
+++ 2390.removing_stars_from_a_string.py
@@ -0,0 +1,12 @@
+#2390.removing_stars_from_a_string
+
+class Solution:
+    def removeStars(self, s: str) -> str:
+        stack=[]
+        for char in s :
+            if char=="*":
+                if stack:
+                    stack.pop()
+            else:
+                stack.append(char)
+        return "".join(stack)
